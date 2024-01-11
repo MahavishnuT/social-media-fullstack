779888ea6873f9e349f490a5b6c56bf969be5bdf
tompujalte <tom.pujalte@gmail.com>
2024-01-11 11:56:15 -0500

    creating and stylizing Register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,7 +3,24 @@ import "./register.scss"
 const Register = () => {
   return(
     <div className="register">
-      Register
+      <div className="card">
+        <div className="card-left">
+          <h1>Tom Social.</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat deleniti fugit architecto unde eveniet dicta dolores excepturi consequuntur impedit velit!</p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="card-right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
