e163abb2cccda4fa0d56902f0f21d0ebe7573ffd
tompujalte <tom.pujalte@gmail.com>
2024-01-11 11:40:16 -0500

    creating and stylizing Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,22 @@ import "./login.scss"
 const Login = () => {
   return(
     <div className="login">
-      Login
+      <div className="card">
+        <div className="card-left">
+          <h1>Hello World.</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat deleniti fugit architecto unde eveniet dicta dolores excepturi consequuntur impedit velit!</p>
+          <span>Do you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="card-right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
